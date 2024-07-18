f926efe5d694c6c785fca298cbcc429d403f6951
Chris Fornia <christopherfornia@gmail.com>
2024-07-18 09:34:16 -0600

    some starter code

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up sessions with cookies
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Set up Handlebars.js engine
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use(require("./controllers/"));
+
+// Sync Sequelize models to the database and start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
